c4f1810a5b9f1ddf8a8160d5388e57391fbb9b2c
Daren Drahoš <daren.drahos@gmail.com>
2023-12-07 02:15:36 +0100

    added class files for test buildings and Grid

--- FactorioClone/FactorioClone.cpp
+++ FactorioClone/FactorioClone.cpp
@@ -1,1 +1,15 @@
+#include "raylib.h"
 
+int main()
+{
+    InitWindow(800, 450, "Factorio Clone");
+    while (!WindowShouldClose())
+    {
+
+        
+        
+
+        
+    }
+    CloseWindow();
+}
